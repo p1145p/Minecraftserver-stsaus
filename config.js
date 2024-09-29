323c27afe1b4108cef1f6fcc5b2c70020789d9ee
p1145p <153534777+p1145p@users.noreply.github.com>
2024-09-30 02:40:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm797677100-9f861708b5f64ed073e12a68',
+    'm797677100-3bc888dbbdef8b9b69f7e5d9',
     'm797755684-5392b4ec4acb08e6cb6913e8',
     'm797755678-79f431bb909bfa401ff9dc4f'
   ],
