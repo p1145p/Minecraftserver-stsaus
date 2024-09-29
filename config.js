44e2507c87f9381fd96943e8fdfd059a483ac5c8
p1145p <153534777+p1145p@users.noreply.github.com>
2024-09-30 01:38:24 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '朋の状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm797677100-9f861708b5f64ed073e12a68'
+  ],
+
+  // 日志天数
+  CountDays: 7,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
