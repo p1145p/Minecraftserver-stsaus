16977e2763ff472afbdc45193046b0176e465337
p1145p <153534777+p1145p@users.noreply.github.com>
2024-09-30 14:24:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm797677100-3bc888dbbdef8b9b69f7e5d9',
-    'm797757150-1ec9113753e19a4548a2d0a9',
+    'm797757150-fb0d15c4bcd010fa5b759cce',
     'm797755684-5392b4ec4acb08e6cb6913e8',
     'm797755678-79f431bb909bfa401ff9dc4f'
   ],
